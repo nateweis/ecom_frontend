948a8b967cfc5f23585d9046ebed2bf4ce7d4dfe
Naftali Weiss <Naftali@PROPUMP.local>
2022-01-10 12:45:49 -0500

    made a basic nav (dosnt redirect yet)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,93 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Badge } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 
-const Containter = styled.div`
-    height: 60px;
-    background-color: black;
-`;
 
 const Navbar = () => {
     return (
         <Containter>
-            Navbar
+            <Wrapper>
+
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        <Input />
+                        <Search style={{color: 'gray', fontSize: '16px'}} />
+                    </SearchContainer>
+                </Left>
+
+                <Center>
+                    <Logo>ART BY DEV</Logo>
+                </Center>
+
+                <Right>
+                    <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color="secondary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItem>
+                </Right>
+
+            </Wrapper>
         </Containter>
     )
 }
+
+// ////////// Style //////////////
+const Containter = styled.div`
+    height: 60px;
+`;
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+// Left Section Styles
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`;
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`
+const SearchContainer = styled.div`
+    border: .5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`
+const Input = styled.input`
+    border: none;
+`
+
+// Center Section Styles
+const Center = styled.div`
+    flex: 1;
+    text-align: center;
+`;
+const Logo = styled.h1`
+    font-weight: bold;
+`
+
+// Right Section Styles
+const Right = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`;
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+`
 
 export default Navbar
