33a3a465960f16094b4408317505f0a71b213c90
Naftali Weiss <Naftali@PROPUMP.local>
2022-01-10 10:34:46 -0500

    added a font and homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Home from './pages/Home'
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 };
 
 export default App;
