e85c632824ea0ef8c477f08b851d0db001053708
Naftali Weiss <Naftali@PROPUMP.local>
2022-01-10 11:33:05 -0500

    added styled componentes instead of css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+
 
 const Home = () => {
     return (
         <div>
-            HomePage
+            <Navbar />
         </div>
     )
 }
