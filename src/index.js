89b12ed76bd04e9d2bd0de746e1339bea69891d3
Naftali Weiss <Naftali@PROPUMP.local>
2022-01-10 10:09:50 -0500

    Made a react boilerplate for general ecom project frontend

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Reducers 
+
+
+const reducer = combineReducers({
+   
+})
+
+// Store
+const store = configureStore({reducer})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
