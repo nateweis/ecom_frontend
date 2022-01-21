176b1798bdee3fceb9ae8087f33796a440626d5c
Naftali Weiss <Naftali@PROPUMP.local>
2022-01-21 10:45:39 -0500

    did a little responsive adjusting to the product list page

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,6 +4,7 @@ import ContactForm from "../components/ContactForm"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import Products from "../components/Products"
+import { mobile } from "../responsive"
 
 const ProductList = () => {
     return (
@@ -64,15 +65,22 @@ const FilterContainer = styled.div`
 `
 const Filter = styled.div` 
     margin: 20px;
+    ${mobile({
+        margin: "0 20px",
+        display: "flex",
+        flexDirection: "column"
+    })}
 `
 const FilterText = styled.span` 
     font-size: 20px;
     font-weight: 600;
     margin-right: 20px;
+    ${mobile({marginRight: "0", fontSize: 18})}
 `
 const Select = styled.select` 
     padding: 10px;
     margin-right: 20px;
+    ${mobile({margin: "12px 0"})}
 `
 const Option = styled.option` 
 
