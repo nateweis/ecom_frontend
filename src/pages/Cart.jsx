59399d6c6ec02a18f41879affc13b9db186e97b2
Naftali Weiss <Naftali@PROPUMP.local>
2022-01-21 09:44:16 -0500

    finnished cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from "@material-ui/icons"
 import styled from "styled-components"
 import Announcement from "../components/Announcement"
 import Footer from "../components/Footer"
@@ -26,20 +27,87 @@ const Cart = () => {
                 <Bottom>
                     <Info>
                         <ProductContainer>
+
                             <ProductDetails>
                                 <Img src={Pic["p1"]} />
                                 <Details>
                                     <ProductName> <b>Product:</b> Painting of Aleph </ProductName>
                                     <ProductId> <b>ID:</b> 345730947 </ProductId>
-                                    <ProductColor />
+                                    <ProductColor color="black" />
                                     <ProductSize> <b>Size:</b> 300 X 350 </ProductSize>
                                 </Details>
                             </ProductDetails>
-                            <PriceDetails>Price</PriceDetails>
+
+                            <PriceDetails>
+
+                                <ProductAmountContainer>
+                                    <Remove />
+                                    <ProductQuantity>2</ProductQuantity>
+                                    <Add />
+                                </ProductAmountContainer>
+                                <ProductPrice>$ 150</ProductPrice>
+
+                            </PriceDetails>
+
+                        </ProductContainer>
+
+                        <Hr />
+
+                        <ProductContainer>
+
+                            <ProductDetails>
+                                <Img src={Pic["p2"]} />
+                                <Details>
+                                    <ProductName> <b>Product:</b> Painting of Bet </ProductName>
+                                    <ProductId> <b>ID:</b> 348509348 </ProductId>
+                                    <ProductColors>
+                                        <ProductColor color="darkblue" />
+                                        <ProductColor color="green" />
+                                    </ProductColors>
+                                    <ProductSize> <b>Size:</b> 300 X 350 </ProductSize>
+                                </Details>
+                            </ProductDetails>
+
+                            <PriceDetails>
+
+                                <ProductAmountContainer>
+                                    <Remove />
+                                    <ProductQuantity>1</ProductQuantity>
+                                    <Add />
+                                </ProductAmountContainer>
+                                <ProductPrice>$ 150</ProductPrice>
+
+                            </PriceDetails>
+
                         </ProductContainer>
                     </Info>
 
-                    <Summary>sum</Summary>
+                    <SummaryContainer>
+                        <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+
+                        <SummaryItem>
+                            <SummaryItemText>Subtotal</SummaryItemText>
+                            <SummaryItemPrice>$ 150.00</SummaryItemPrice>
+                        </SummaryItem>
+
+                        <SummaryItem>
+                            <SummaryItemText>Estimated Shipping</SummaryItemText>
+                            <SummaryItemPrice>$ 5.60</SummaryItemPrice>
+                        </SummaryItem>
+
+                        <SummaryItem>
+                            <SummaryItemText>Shipping Discount</SummaryItemText>
+                            <SummaryItemPrice>$ -5.60</SummaryItemPrice>
+                        </SummaryItem>
+
+                        <SummaryItem type="total">
+                            <SummaryItemText>Total</SummaryItemText>
+                            <SummaryItemPrice>$ 150.00</SummaryItemPrice>
+                        </SummaryItem>
+
+                        <Button>CHECKOUT NOW</Button>
+                    </SummaryContainer>
+
                 </Bottom>
             </Wrapper>
 
@@ -86,38 +154,106 @@ const Bottom = styled.div`
     display: flex;
     justify-content: space-between;
 `
+const Hr =styled.hr` 
+    background-color: #eee;
+    border: none;
+    height: 1px;
+    margin: 10px 0;
+`
 const Info = styled.div`
     flex:3;
 `
-const ProductContainer = styled.div` 
 
+// Product Details
+const ProductContainer = styled.div` 
+    display: flex;
+    justify-content: space-between;
 `
 const ProductDetails = styled.div` 
-
+    flex: 2;
+    display: flex;
 `
 const Img = styled.img` 
-
+    width: 200px;
 `
 const Details = styled.div` 
-
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
 `
-const ProductName = styled.span` 
+const ProductName = styled.span` `
 
-`
-const ProductId = styled.span` 
+const ProductId = styled.span` `
 
+
+const ProductColors = styled.div` 
+    display: flex;
+    align-items: center;
 `
 const ProductColor = styled.div` 
-
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    margin: 0 5px;
+    background-color: ${props => props.color};
 `
-const ProductSize = styled.span` 
+const ProductSize = styled.span` `
 
-`
+// Price 
 const PriceDetails = styled.div` 
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+const ProductAmountContainer = styled.div` 
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`
+const ProductQuantity = styled.div` 
+    font-size: 24px;
+    margin: 5px;
+`
+const ProductPrice = styled.div` 
+    font-size: 30px;
+    font-weight: 200;
+`
+
+// Summary
+const SummaryContainer = styled.div`
+    flex:1;
+    border: .5px solid lightgray;
+    padding: 20px;
+    border-radius: 10px;
+    height: 50vh;
+`
+const SummaryTitle = styled.h1`
+    font-weight: 200;
+`
+const SummaryItem = styled.div` 
+    margin: 30px 0;
+    display: flex;
+    justify-content: space-between;
+    font-weight: ${props => props.type === "total" && "500"} ;
+    font-size: ${props => props.type === "total" && "24px"} ;
+`
+const SummaryItemText = styled.span` 
 
 `
-const Summary = styled.div`
-    flex:1;
+const SummaryItemPrice = styled.span` 
+
+`
+const Button = styled.button` 
+    width: 100% ;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    cursor: pointer;
+    border: none;
+    font-weight: 600;
 `
 
 export default Cart
